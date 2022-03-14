209a25d40797343175effc5097b0196fa6223fdd
lawrencep01 <lvp2dun@virginia.edu>
2022-03-14 17:27:21 -0400

    Updated heroku test settings

--- GroupB24/settings.py
+++ GroupB24/settings.py
@@ -24,6 +24,7 @@ from pathlib import Path
 import sys
 import django_heroku
 
+django_heroku.settings(locals(), test_runner=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
