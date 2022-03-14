6e386f1dc115917810c4452dd1c0e447a8e5e89b
lawrencep01 <lvp2dun@virginia.edu>
2022-03-14 16:16:29 -0400

    Revert to "modified database settings" commit

--- GroupB24/settings.py
+++ GroupB24/settings.py
@@ -109,18 +109,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
-
 
 # Password validation
 #<<<<<<< HEAD
