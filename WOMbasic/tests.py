91a3020a3c3fbcfcafd4f7e73235b58354b5f38d
lawrencep01 <lvp2dun@virginia.edu>
2022-03-14 11:31:57 -0400

    Created basic tests for models.py

--- WOMbasic/tests.py
+++ WOMbasic/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from . models import Recipe
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.recipe = Recipe.objects.create(recipe_text='test')
+
+    def test_recipe_model(self):
+        t = self.recipe
+        self.assertTrue(isinstance(t, Recipe))
+        self.assertEqual(str(t), 'test')
