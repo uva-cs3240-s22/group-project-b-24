1bfa5a5c0dcdd48222c257e6157fcdca25bc4c38
Kevin Carlson <kmc6am@virginia.edu>
2022-03-24 16:26:13 -0400

    add and delete recipe functionality

--- WOMbasic/urls.py
+++ WOMbasic/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
-from .views import HomeView
 
 app_name = "WOMbasic"
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipe-details'),
-    path('submit/', views.insert, name='submit')
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe-details'),
+    path('submit/', views.SubmitRecipe.as_view(), name='submit'),
+    path('recipe/<int:pk>/delete/', views.DeleteRecipe.as_view(), name='delete')
 ]
--- WOMbasic/views.py
+++ WOMbasic/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.http import HttpResponse
 from . models import Recipe
+from django.urls import reverse_lazy
 
-
-def insert(request):
-    try:
-        sub_text = request.POST['nameOfRecipe']
-    except KeyError:
-        sub_text = "oopsies"
-        return render(request, 'WOMbasic/submit.html')
-    else:
-        r = Recipe()
-        r.recipe_name = request.POST['nameOfRecipe']
-        r.save()
-        return render(request, 'WOMbasic/recipe/list.html')
-
+class SubmitRecipe(CreateView):
+    model = Recipe
+    template_name = 'WOMbasic/submit.html'
+    fields = '__all__'
 
 class RecipeDetailView(DetailView):
     model = Recipe
@@ -24,3 +16,8 @@ class RecipeDetailView(DetailView):
 class HomeView(ListView):
     model = Recipe
     template_name = 'WOMbasic/home.html'
+
+class DeleteRecipe(DeleteView):
+    model = Recipe
+    template_name = 'WOMbasic/delete_recipe.html'
+    success_url = reverse_lazy('WOMbasic:home')
