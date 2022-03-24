2db45458d0fbb3051c8db7540bffc9e6d0d633d9
Kevin Carlson <kmc6am@virginia.edu>
2022-03-23 21:11:43 -0400

    updated models and views

--- WOMbasic/urls.py
+++ WOMbasic/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import HomeView
 
 app_name = "WOMbasic"
 urlpatterns = [
     path('recipe/list', views.listView, name='listView'),
     path('recipe/', views.insert, name='submit'),
-    path('', views.welcomeView, name='welcome'),
+    path('', views.HomeView.as_view(), name='home'),
 ]
--- WOMbasic/views.py
+++ WOMbasic/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from . models import Recipe
 
 
 def insert(request):
     try:
-        sub_text = request.POST['recipe']
+        sub_text = request.POST['nameOfRecipe']
     except KeyError:
         sub_text = "oopsies"
         return render(request, 'WOMbasic/submit.html')
     else:
         r = Recipe()
-        r.recipe_text = request.POST.get('recipe', "empty")
+        r.recipe_name = request.POST['nameOfRecipe']
         r.save()
-        return render(request, 'WOMbasic/submit.html')
+        return render(request, 'WOMbasic/recipe/list.html')
+
 
 def listView(request):
     latest_recipe_list = Recipe.objects.order_by()
     context = {'latest_recipe_list': latest_recipe_list,}
     return render(request, 'WOMbasic/recipe.html', context)
 
-def welcomeView(request):
-    return render(request, 'WOMbasic/welcome.html')
+class HomeView(ListView):
+    model = Recipe
+    template_name = 'WOMbasic/home.html'
