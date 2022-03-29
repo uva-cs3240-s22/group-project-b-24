374ee5d99c1830e4fb344e077a5e82f66ab65914
lawrencep01 <lvp2dun@virginia.edu>
2022-03-29 13:04:01 -0400

    Added url to recipe

--- WOMbasic/tests.py
+++ WOMbasic/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
+from datetime import datetime
+from django.contrib.auth.models import User
 from . models import Recipe
 # Create your tests here.
-class ModelTesting(TestCase):
+class RecipeTest(TestCase):
 
     def setUp(self):
-        self.recipe = Recipe.objects.create(recipe_text='test')
+        testUser = User.objects.createUser()
+        self.recipe = Recipe.objects.create(publisher = User, recipe_name = 'test', date_published = datetime.now, recipe_description ='testdesc')
 
     def test_recipe_model(self):
         t = self.recipe
         self.assertTrue(isinstance(t, Recipe))
-        self.assertEqual(str(t), 'test')
