30e52e8762b03078a7b97f390d28737ff1a74ff1
Kevin Carlson <kmc6am@virginia.edu>
2022-03-30 16:45:14 -0400

    static files fix 4

--- GroupB24/settings.py
+++ GroupB24/settings.py
@@ -206,9 +206,9 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
     )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
