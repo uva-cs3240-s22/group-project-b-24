a472430173c4c6b3f56881d8180b0b7ee3b0bc56
lawrencep01 <lvp2dun@virginia.edu>
2022-04-04 15:19:03 -0400

    Disabled original static URL

--- GroupB24/settings.py
+++ GroupB24/settings.py
@@ -164,14 +164,14 @@ USE_TZ = True
 #<<<<<<< HEAD
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 #=======
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
