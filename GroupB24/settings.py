386c4439b235e289075bb699f92b362a5a52dc4a
lawrencep01 <lvp2dun@virginia.edu>
2022-04-04 15:15:17 -0400

    Added AWS S3 hosting for images

--- GroupB24/settings.py
+++ GroupB24/settings.py
@@ -205,12 +205,26 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = '/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_FILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+#MEDIA_ROOT = '/media'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-django_heroku.settings(locals(), test_runner=False)
+AWS_ACCESS_KEY_ID = 'AKIARDRHUHLVNYSM424R'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'b24projectbucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' %AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_LOCATION = 'static'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'GroupB24.storages.MediaStore'
+
+
+django_heroku.settings(locals(), test_runner=False, staticfiles=False)
