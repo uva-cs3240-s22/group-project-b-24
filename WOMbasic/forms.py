90204881ca14fbf76c4397fda49d576b69b51560
Kevin Carlson <kmc6am@virginia.edu>
2022-04-11 12:07:42 -0400

    submit recipes only as the person logged in as

--- WOMbasic/forms.py
+++ WOMbasic/forms.py
@@ -6,10 +6,10 @@ from .models import Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ('publisher', 'recipe_name', 'recipe_description', 'recipe_image')
+        fields = ('recipe_name', 'recipe_description', 'recipe_image')
 
         widgets = {
-            'publisher': forms.Select(attrs={'class': 'form-control'}),
+            #   'publisher': forms.TextInput(attrs={'class': 'form-control'}),
             'recipe_name': forms.TextInput(attrs={'class': 'form-control'}),
             'recipe_description': forms.Textarea(attrs={'class': 'form-control'}),
             'recipe_image': forms.FileInput(attrs={'class': 'form-control'}),
