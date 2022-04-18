a79f43e2e4d6a152c549930108c459afa747b5e8
silkykay <mlt6kps@virginia.edu>
2022-04-18 16:23:28 -0400

    fixed

--- WOMbasic/admin.py
+++ WOMbasic/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Ingredients
 
-class IngredientsInLineAdmin(admin.TabularInline):
-    model = Ingredients
-
-class RecipeAdmin(admin.ModelAdmin):
-    inlines = [IngredientsInLineAdmin]
-
-admin.site.register(Recipe, RecipeAdmin)
+# Register your models here.
+from.models import Recipe
+admin.site.register(Recipe)
