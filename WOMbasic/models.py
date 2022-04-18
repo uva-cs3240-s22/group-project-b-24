a099c1bca1d8e29e75de829d909f85767bd7780f
silkykay <mlt6kps@virginia.edu>
2022-04-18 14:56:06 -0400

    changes to models

--- WOMbasic/models.py
+++ WOMbasic/models.py
@@ -30,4 +30,12 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse('WOMbasic:recipe-details', args=(str(self.pk)))
+class Comment(models.Model):
+    post = models.ForeignKey(Recipe, related_name ='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return '%s - %s' %(self.post.title, self.name)
 
